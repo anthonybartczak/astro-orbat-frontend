43544526d3b02a851676b137097bdf5887118776
anthonybartczak <to0oni456@gmail.com>
2023-04-26 16:30:20 +0200

    First itteration of platoon view

---
+++ src/components/PlatoonStructure.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ms from 'milsymbol';
+import { GiPerson, GiRank3, GiM3GreaseGun } from 'react-icons/gi'
+
+export default function PlatoonStructure({ platoon }:any) {
+
+	var platoonSymbol = new ms.Symbol(
+		"SFG-UCI----D",
+		{
+			colorMode: "Dark"
+		}
+	).asSVG();
+
+	return (
+		<>
+			<ul role="list" className="flex flex-col gap-4">
+				{platoon.map((item: any) => (
+					<li className='text-2xl flex flex-col'>
+					{
+						item.count > 1 ? item.count + " x " + item.name : item.name
+					}
+					{item.units.map((unit: any) => (
+						<div className='flex font-light'>
+							<GiPerson className='text-3xl p-1'/>
+							<span className='text-xl'>{unit.name}</span>
+							<div className='flex pl-3'>
+								<GiRank3 className='text-3xl p-1'/><div className='text-base pl-0 p-1'>{unit.rank}</div>
+							</div>
+							<div className='flex pl-3'>
+								<GiM3GreaseGun className='text-3xl p-1'/><div className='text-base pl-0 p-1'>
+									{unit.weapons.join(', ')}
+								</div>
+							</div>
+						</div>
+					))}
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
